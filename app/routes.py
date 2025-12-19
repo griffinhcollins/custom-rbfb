30a65d31d652965772c65dedc3feceed285b832b
NervousDendrite <griffinhoycollins@gmail.com>
2025-12-19 11:17:41 +1100

    Added share page after creating new RBFB

--- app/routes.py
+++ app/routes.py
@@ -17,14 +17,17 @@ def new():
     if form.validate_on_submit():
         rbfb = RBFB(topic=form.topic.data)
         rbfb.urlval = hex(hash(str(rbfb.id)))
-        flash(f"Created RBFB with topic {form.topic.data}, url=http://127.0.0.1:5000/view/{rbfb.urlval}")
+        flash(
+            f"Created RBFB with topic {form.topic.data}, share here: http://127.0.0.1:5000/view/{rbfb.urlval}"
+        )
         db.session.add(rbfb)
         for question in form.questions:
             c = Candidate(value=question.entry.data, real=question.real.data == "r", parent=rbfb)
             db.session.add(c)
         db.session.commit()
-        return redirect(url_for("index"))
+        return redirect(url_for("share", urlval=rbfb.urlval))
     return render_template("new.html", title="New RBFB", form=form)
+
 
 @app.route("/view/<urlval>")
 def view(urlval):
@@ -34,3 +37,9 @@ def view(urlval):
     for candidate in db.session.scalars(rbfb.candidates.select()).all():
         candidates.append((candidate.value, candidate.real))
     return render_template("view.html", topic=rbfb.topic, candidates=candidates)
+
+
+@app.route("/share/<urlval>")
+def share(urlval):
+    shareurl = f"http://127.0.0.1:5000/view/{urlval}"
+    return render_template("share.html", url=shareurl)
