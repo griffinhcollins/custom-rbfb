98fd88bee01e1e8ebff793ea369f531bc8ad9ae0
Griffin Collins <griffin.collins@dewr.gov.au>
2026-04-29 10:28:20 +1000

    Removed numpy dependency

--- app/imagegen.py
+++ app/imagegen.py
@@ -1,24 +1,14 @@
 from PIL import Image, ImageDraw
 from simpilfont import SimPILFont
-import numpy as np
 import colorsys
 
 
-def generate_background(hue, width, height):
-    a = np.zeros((width, height, 3), dtype=np.uint8)
-    rgb = colorsys.hsv_to_rgb(hue, 0.4, 0.7)
-    for i in range(3):
-        a[:, :, i].fill(rgb[i] * 255)
-    return Image.fromarray(a)
 
 
 def generate_scorecard(rgb: str, topic: str, score: str):
     # Generate background
-    rgb = [int(float(d)) for d in rgb.split(" ")]
-    array = np.zeros((400, 500, 3), dtype=np.uint8)
-    for i in range(3):
-        array[:, :, i].fill(rgb[i])
-    img = Image.fromarray(array)
+    rgb = tuple(int(float(d)) for d in rgb.split(" "))
+    img = Image.new('RGB', (500, 400), rgb)
     draw = ImageDraw.Draw(img)
 
     # Sort out fonts
